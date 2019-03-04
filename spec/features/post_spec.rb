aadd49874d8cefe6930bfbd6be0d158c9902dd63
Taylor Williams <taylor@thewilliams.net>
2019-03-04 20:39:31 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,7 +12,7 @@ describe 'post_path' do
   end
 
   it 'shows the appropriate post information' do
-    visit post_path(@post)
+    visit posts_path(@post)
     expect(page).to have_css('h1', text: 'My Post'), 'show.html is not rendering correct content'
   end
 end
@@ -24,12 +24,12 @@ describe 'post_controller' do
   end
 
   it 'responds to json format request' do
-    visit post_path(id: @post.id, format: :json)
+    visit posts_path(id: @post.id, format: :json)
     expect(page.status_code).to eq(200)
   end
 
   it 'returns appropriate json content' do
-    visit post_path(id: @post.id, format: :json)
+    visit posts_path(id: @post.id, format: :json)
     expect(page.html).not_to include('<!DOCTYPE html>'), 'expected JSON to be returned, got HTML instead'
     expect(JSON.parse(page.html)).to eq('author' => { 'name' => 'Cormac McCarthy' }, 'description' => 'My post desc', 'id' => 1, 'title' => 'My Post'), 'returned JSON does not match expected format/may be have incorrect content'
   end
